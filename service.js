bd08f0f1b5ceebf7f9ee5d8599165263eb15da06
basilioarth <basilio.arth@gmail.com>
2021-06-06 17:34:41 -0300

    Atualizando a estrutura do emails.json, atualizando req.statusCode e realizando tratamento para todos os gets

--- service.js
+++ service.js
@@ -3,87 +3,123 @@ const emails = require('./emails.json');
 
 exports.getUser = function(req, res) {
     const reqUrl = url.parse(req.url, true);
-    var response = emails[reqUrl.query.name.toLowerCase()]
 
-    if(response !== undefined) {
-        res.statusCode = 200;
+    if(reqUrl.query.name){
+        var response = emails.users.filter((user) => {
+            return user.name == reqUrl.query.name.toLowerCase()
+        })
+        
+        if(response.length === 0) {
+            res.statusCode = 404;
+        } else {
+            res.statusCode = 200;
+        }
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(response));
     } else {
-        res.statusCode = 404;
+        res.statusCode = 400;
         res.setHeader('Content-Type', 'application/json');
-        res.end('Usuário não existente');
+        res.end('Invalid-Request');
     }
-}
+};
 
 exports.getAllUserEmails = function(req, res) {
     const reqUrl = url.parse(req.url, true);
-    var response = '';
 
     if(reqUrl.query.name){
-        response = emails[reqUrl.query.name.toLowerCase()]
+        var response = emails[reqUrl.query.name.toLowerCase()]
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(response));
+        if(response === undefined) {
+            res.statusCode = 404
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({}));
+        } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(response));
+        }
     } else {
-        res.statusCode = 404;
+        res.statusCode = 400;
         res.setHeader('Content-Type', 'application/json');
-        res.end('Invalid Request');
+        res.end('Invalid-Request');
     }
 };
 
 exports.getAllSendedEmails = function(req, res) {
     const reqUrl = url.parse(req.url, true);
-    var response = '';
 
     if(reqUrl.query.name){
-        response = emails[reqUrl.query.name.toLowerCase()]["sent"]
+        var response = emails[reqUrl.query.name.toLowerCase()]
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(response));
+        if(response === undefined) {
+            res.statusCode = 404
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({}));
+        } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(response["sent"]));
+        }
     } else {
-        res.statusCode = 404;
+        res.statusCode = 400;
         res.setHeader('Content-Type', 'application/json');
-        res.end('Invalid Request');
+        res.end('Invalid-Request');
     }
 };
 
 exports.getAllRecievedEmails = function(req, res) {
     const reqUrl = url.parse(req.url, true);
-    var response = '';
 
     if(reqUrl.query.name){
-        response = emails[reqUrl.query.name.toLowerCase()]["inbox"]
+        var response = emails[reqUrl.query.name.toLowerCase()]
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(response));
+        if(response === undefined) {
+            res.statusCode = 404
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({}));
+        } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(response["inbox"]));
+        }
     } else {
-        res.statusCode = 404;
+        res.statusCode = 400;
         res.setHeader('Content-Type', 'application/json');
-        res.end('Invalid Request');
+        res.end('Invalid-Request');
     }
 };
 
 exports.getUserEmail = function(req, res) {
     const reqUrl = url.parse(req.url, true);
-    var response = '';
 
     if(reqUrl.query.name && reqUrl.query.id && reqUrl.query.class){
-        response = emails[reqUrl.query.name.toLowerCase()][reqUrl.query.class]
-        response = response.filter((email) => {
-            return email.id == reqUrl.query.id
-        })
+        var response = emails[reqUrl.query.name.toLowerCase()]
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(response));
+        if(response === undefined) {
+            res.statusCode = 404
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({}));
+        } else if(response[reqUrl.query.class] === undefined){
+            res.statusCode = 404
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({}));
+        } else {
+            response = response[reqUrl.query.class].filter((email) => {
+                return email.id == reqUrl.query.id
+            })
+            
+            if(response.length === 0) {
+                res.statusCode = 404;
+            } else {
+                res.statusCode = 200;
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(response));
+        }
     } else {
-        res.statusCode = 404;
+        res.statusCode = 400;
         res.setHeader('Content-Type', 'application/json');
-        res.end('Invalid Request');
+        res.end('Invalid-Request');
     }
 };
 
@@ -124,7 +160,7 @@ exports.sendEmail = function(req, res) {
 };
 
 exports.invalidRequest = function(req, res) {
-    res.statusCode = 404;
+    res.statusCode = 400;
     res.setHeader('Content-Type', 'application/json');
-    res.end('Invalid Request');
+    res.end('Invalid-Request');
 };
